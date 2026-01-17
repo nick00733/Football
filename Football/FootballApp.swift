0938c654e6f17f68debd2e6761a1702986dfb80f
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-17 13:47:51 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Nick Nedjat on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
